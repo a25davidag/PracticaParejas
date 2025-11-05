5836d6775dd82d3d09f392e39ef0d55edc26dab9
David Arranz González <a25davidag@teis.intranet>
2025-11-05 16:28:28 +0100

    ProductoUsuario

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido");
+        Usuario u = new Usuario();
+        u.nombre = "David";
+        u.mostrarInformacion();
     }
 }
